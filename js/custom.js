39e6353f786f7754ee32f97e1419a7ce1aafd20a
FI <step21@devtal.de>
2015-01-08 02:44:37 +0100

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+// custom.js
+
+//animation & display of small 'sheet' to big sheet
+
+//make one function for all?
+/* $(function() {
+	$( "#bp" ).click(function() {
+		$( "#effect" ).toggleClass( "newClass", 1000 );
+		});
+	}); */
+// In case we need animated enlargement ...
+/* $(function() {
+	$( '#bp' ).click(function() {
+		$( '#bp' ).toggleClass( "12u 4u no-collapse", 1000 );
+		$( '#access' ).fadeToggle();
+		$( '#review' ).fadeToggle();
+
+	});
+	$( '#access' ).click(function() {
+		$( '#access' ).toggleClass( "12u 4u", 1000 );
+		$( '#bp' ).fadeToggle();
+		$( '#review' ).fadeToggle();
+	});
+	$( '#review' ).click(function() {
+		$( '#review' ).toggleClass( "12u 4u", 1000 );
+		$( '#access' ).fadeToggle();
+		$( '#bp' ).fadeToggle();
+	});
+}); */
+
